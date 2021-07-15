27ba19ba77f7883a5c55accf861cd3bb280c6b1d
G2killer <2428183599@qq.com>
2021-07-15 08:40:41 +0800

    更新

---
+++ pta/Basic programming/支票面额.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+int main()
+{
+    int n;
+	scanf("%d",&n);
+	int flag=0;
+	for(int y=0;y<50;y++)//y元的控制条件
+	{
+		for(int f=0;f<100;f++)
+		{
+			if(98*f-199*y==n)
+			{
+				printf("%d.%d",y,f);
+				flag=1;
+				break;
+			}
+		}
+	}
+	if(flag==0)//判断循环结束时是否找到相应的y和f值
+	{
+		printf("No Solution");
+	}	
+	return 0;
+}
