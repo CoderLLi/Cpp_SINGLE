762d0a78e90934ad532021a1c5454322470f3543
G2killer <2428183599@qq.com>
2021-10-23 18:41:51 +0800

    update

---
+++ oj/1005.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T;
+char s[200];
+string ss;
+int main() {
+	scanf("%d", &T);
+	getchar();
+	while (T--) {
+
+		getline(cin, ss);
+		cout << ss << endl;
+	}
+	return 0;
+}
